9597d5c27a696adaa4fb05d202f2838d4fa8e86b
Yisan Huang <anonymousyisan@gmail.com>
2025-12-11 14:50:29 +0800

    feat: update

--- core/aiSubtitle.py
+++ core/aiSubtitle.py
@@ -1,4 +1,10 @@
 import whisper
+import json
+
+
+def write_to_file(text: str, file_path: str):
+    with open(file_path, "w") as f:
+        f.write(text)
 
 
 class SubtitleConverter:
@@ -7,7 +13,8 @@ class SubtitleConverter:
 
     def convert_subtitle(self, audio_path: str):
         result = self.model.transcribe(audio_path)
+        write_to_file(json.dumps(result), "result.json")
         text_list = [i["text"] for i in result["segments"]]
         # text_list 去重
-        text_list = list(set(text_list))
-        return "\n".join(text_list)
+        text_list_uniq = sorted(list(set(text_list)), key=text_list.index)
+        return "\n".join(text_list_uniq)
